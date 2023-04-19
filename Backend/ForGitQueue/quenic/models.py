99cece2429b8c21c20e2f66820b49da0441cdd41
Andrii Myrosh <andriymyrosh@gmail.com>
2023-04-19 16:21:08 +0300

    Added UserProfile

--- Backend/ForGitQueue/quenic/models.py
+++ Backend/ForGitQueue/quenic/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class UserProfile(AbstractUser):
+    nickName = models.CharField(max_length=255)
+    joined_date = models.DateField()
+    email = models.CharField(max_length=255)
+
 class Queue(models.Model):
-    creator = models.ForeignKey(User, on_delete=models.PROTECT, related_name='queues_created')
+    creator = models.ForeignKey(UserProfile, on_delete=models.PROTECT, related_name='queues_created')
     name = models.CharField(max_length=255)
     description = models.TextField()
-    users = models.ManyToManyField(User, related_name='queues_joined')
+    users = models.ManyToManyField(UserProfile, related_name='queues_joined')
 
     def __str__(self):
         return self.name
-
