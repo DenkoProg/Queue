e4bccfc5d63a755d9ed04996946b4cd38f71dab2
DenkoProg <kowaldenysproekt@gmail.com>
2023-04-19 15:16:02 +0300

    Create Queue model

--- Backend/ForGitQueue/quenic/models.py
+++ Backend/ForGitQueue/quenic/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Queue(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.PROTECT, related_name='queues_created')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    users = models.ManyToManyField(User, related_name='queues_joined')
+
+    def __str__(self):
+        return self.name
+
