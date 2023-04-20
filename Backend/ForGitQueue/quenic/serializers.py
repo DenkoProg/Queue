588b58dc0b680d398b1f2c496060a6cf6401e46f
DenkoProg <kowaldenysproekt@gmail.com>
2023-04-20 21:36:07 +0300

    Create REST API and implement default get, post. Delete UserProfile module because of conflict. Create superuser to enter standard django admin menu and view database here

---
+++ Backend/ForGitQueue/quenic/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Queue
+
+class QueueSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Queue
+        fields = ('creator', 'name', 'description', 'users')
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email')
