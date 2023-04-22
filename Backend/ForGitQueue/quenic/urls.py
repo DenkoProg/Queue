a7d82a1620c8739936fbbc937506c1488804c029
Andrii Myrosh <andriymyrosh@gmail.com>
2023-04-22 11:25:35 +0300

    Fixed DELETE endpoint

--- Backend/ForGitQueue/quenic/urls.py
+++ Backend/ForGitQueue/quenic/urls.py
@@ -11,5 +11,7 @@ router.register(r'queue_memberships', views.QueueMembershipViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',
-                              namespace = 'rest_framework'))
+                              namespace = 'rest_framework')),
+    path('queues/<int:queue_id>/delete/', views.delete_queue, name='delete_queue'),
+    path('queues/create/', views.create_queue, name='create_queue'),
 ]
