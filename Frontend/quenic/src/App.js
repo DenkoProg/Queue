d3678f36c991f22ca7dcf6a99dc567490d150e43
DenkoProg <kowaldenysproekt@gmail.com>
2023-04-28 23:14:07 +0300

    Build DescriptionComponent and fix SidePanel

--- Frontend/quenic/src/App.js
+++ Frontend/quenic/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import HeaderComponent from "./Components/HeaderComponent/HeaderComopnent";
 import HomeComponent from "./Components/HomeComponents/HomeComponent";
 import SidePanel from "./Components/SidePanel/SidePanel";
+import Description from "./Components/ModalComponents/DescriptionComponent/Description";
 
 function App() {
   return (
     <div className="App">
       <SidePanel />
       <HeaderComponent />
-      {/*<HomeComponent />*/}
+        <Description />
     </div>
   );
 }
---
+++ Frontend/quenic/src/Components/ModalComponents/DescriptionComponent/Description.js
@@ -0,0 +1,26 @@
+import classes from './Description.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Button from 'react-bootstrap/Button';
+import Link from './images/Link.png'
+import Code from './images/Code.png'
+import Exit from './images/Exit.png'
+
+function Description() {
+    return (
+        <div className={classes.mainContainer}>
+            <img src={Exit} className={classes.exitImage}/>
+            <div className={classes.textBlock}>
+                <div className={classes.queueDescription}>Queue Description</div>
+                <div className={classes.paragraph}>Commodo eget a et dignissim dignissim morbi vitae, mi. Mi aliquam sit
+                    ultrices enim cursus. Leo sapien, pretium duis est eu volutpat interdum eu non. Odio eget nullam
+                    elit laoreet. Libero at felis nam at orci venenatis rutrum nunc. Etiam mattis ornare pellentesque
+                    iaculis enim.
+                </div>
+            </div>
+            <Button className={classes.codeButton} variant="outline-secondary"><img src={Code} className={classes.codeImage}></img>Q763GFY2</Button>
+            <Button className={classes.linkButton} variant="dark"><img src={Link} className={classes.linkImage}></img>Copy Link</Button>
+        </div>
+    )
+}
+
+export default Description
