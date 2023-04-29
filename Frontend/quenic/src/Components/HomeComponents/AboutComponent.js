4a6377d12510ef32ec9b3ecab91e0beaac6bf09e
yaiechnyk-oleh <kindercorporation13@gmail.com>
2023-04-29 14:03:14 +0300

    Backup

---
+++ Frontend/quenic/src/Components/HomeComponents/AboutComponent.js
@@ -0,0 +1,24 @@
+import './AboutComponent.css'
+function AboutComponent () {
+    return(
+        <div className = "about-wrapper">
+            <div className = "about-container">
+                <div className = "about-container-title">
+                    <p className = "about-container-text">How does EmailShareViaLink work?</p>
+                    <button className = "about-container-button">+</button>
+                </div>
+                <span className = "about-container-details">
+                    We design and produce premium quality furniture and accessories for
+                    living,dining, sleeping, home-office, and also for commercial spaces.
+                    For the manufacture of our products, we use only high-quality
+                    EmailShareViaLink is a tool that allows you to share an email message
+                    with others via a link. Simply forward the email to a special address
+                    provided by EmailShareViaLink, and you will receive a link to the email
+                    that can be shared with anyone you wish.y raw materials.
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default AboutComponent;
