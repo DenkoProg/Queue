77a64c505d317edfab8e54b715da6383f8f1f306
Andrii Myrosh <andriymyrosh@gmail.com>
2023-04-30 21:03:00 +0300

    Make API documantation

--- Backend/ForGitQueue/ForGitQueue/settings.py
+++ Backend/ForGitQueue/ForGitQueue/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'dj_rest_auth.registration',
+    'drf_yasg',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
