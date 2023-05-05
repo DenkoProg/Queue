cf2b827e368a5a90058812781911a28ea97b8ccc
Andrii Myrosh <andriymyrosh@gmail.com>
2023-05-05 08:37:16 +0300

    Change urls

--- Backend/ForGitQueue/ForGitQueue/urls.py
+++ Backend/ForGitQueue/ForGitQueue/urls.py
@@ -20,10 +20,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('quenic.urls')),
+    path('', include('quenic.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
