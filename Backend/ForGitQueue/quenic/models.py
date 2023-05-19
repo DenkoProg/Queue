b4be8b0a15897dfb19237495d4162cb87fbbc2c9
DenkoProg <kowaldenysproekt@gmail.com>
2023-05-19 10:13:04 +0300

    Queue shows amount of users in it through QueueMembership and doesn't shows the users itself

--- Backend/ForGitQueue/quenic/models.py
+++ Backend/ForGitQueue/quenic/models.py
@@ -7,13 +7,17 @@ class Queue(models.Model):
     creator = models.ForeignKey(User, on_delete=models.PROTECT, related_name='queues_created')
     name = models.CharField(max_length=255)
     description = models.TextField()
-    users = models.ManyToManyField(User, related_name='queues_joined')
+
 
     def __str__(self):
         return self.name
 
     def is_creator(self, user):
         return self.creator == user
+
+    def count_users(self):
+        return self.queuemembership_set.count()
+
 
 
 class QueueMembership(models.Model):
--- Backend/ForGitQueue/quenic/serializers.py
+++ Backend/ForGitQueue/quenic/serializers.py
@@ -3,9 +3,13 @@ from django.contrib.auth import get_user_model
 from .models import Queue, QueueMembership
 
 class QueueSerializer(serializers.ModelSerializer):
+    user_count = serializers.SerializerMethodField()
     class Meta:
         model = Queue
-        fields = ('code', 'id', 'creator', 'name', 'description', 'users')
+        fields = ('code', 'id', 'creator', 'name', 'description', 'user_count')
+
+    def get_user_count(self, obj):
+        return obj.count_users()
 
 class UserSerializer(serializers.ModelSerializer): # new
     class Meta:
